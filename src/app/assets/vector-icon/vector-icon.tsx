bf03d173117434465e891f3364d89ded80a44643
jrhe1213 <jiarong1213@gmail.com>
2022-07-09 17:51:05 -0400

    chore(mobile): vecotr icon

--- src/app/assets/vector-icon/vector-icon.tsx
+++ src/app/assets/vector-icon/vector-icon.tsx
@@ -1,25 +1,25 @@
-import React, { memo, MemoExoticComponent, NamedExoticComponent } from 'react';
+import React, { memo, MemoExoticComponent, NamedExoticComponent } from "react";
 
-import isEqual from 'react-fast-compare';
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import isEqual from "react-fast-compare";
+import { createIconSetFromIcoMoon } from "react-native-vector-icons";
 
-import { CustomOmit } from '@common';
-import { Colors, useTheme } from '@theme';
-import { IconProps } from 'react-native-vector-icons/Icon';
+import { CustomOmit } from "@common";
+import { Colors, useTheme } from "@theme";
+import { IconProps } from "react-native-vector-icons/Icon";
 
-import { ICONS } from './icon-name';
+import { ICONS } from "./icon-name";
 
 const VectorIconBase = createIconSetFromIcoMoon(
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('./selection.json'),
-  'icons',
-  'icons.ttf',
+  require("./selection.json"),
+  "icons",
+  "icons.ttf"
 );
 
 const Icon = memo(VectorIconBase, isEqual);
 export type VectorIconIcon = keyof typeof ICONS;
 
-type VectorIconProps = CustomOmit<IconProps, 'name'> & {
+type VectorIconProps = CustomOmit<IconProps, "name"> & {
   icon: VectorIconIcon;
   colorTheme?: keyof Colors;
 };
