0e70479b0dc28f89f4dc876c6a5356fd8890cb04
jrhe1213 <jiarong1213@gmail.com>
2022-07-09 21:13:56 -0400

    chore(mobile): switch ui

--- src/app/library/components/switch/constants.ts
+++ src/app/library/components/switch/constants.ts
@@ -3,12 +3,12 @@ export const WIDTH = 28;
 export const MARGIN = 2;
 export const TRACK_HEIGHT = 16;
 // colors
-export const ON_COLOR = '#34C759';
-export const ON_TRACK_COLOR = 'rgba(52, 199, 89, 0.4)';
-export const OFF_TRACK_COLOR = 'rgba(0, 0, 0, 0.3)';
-export const OFF_COLOR = '#e6e6e6';
-export const BORDER_OFF_COLOR = 'rgba(0, 0, 0, 0.05)';
-export const SHADOW_COLOR = 'rgba(0, 0, 0, 0.3)';
+export const ON_COLOR = "#34C759";
+export const ON_TRACK_COLOR = "rgba(52, 199, 89, 0.4)";
+export const OFF_TRACK_COLOR = "rgba(0, 0, 0, 0.3)";
+export const OFF_COLOR = "#e6e6e6";
+export const BORDER_OFF_COLOR = "rgba(0, 0, 0, 0.05)";
+export const SHADOW_COLOR = "rgba(0, 0, 0, 0.3)";
 
 // ios
 export const THUMB_SIZE_IOS = 20;
