f04fd9a02c7bbddabd4ec51052df4d7c24b9e726
jrhe1213 <jiarong1213@gmail.com>
2022-08-06 01:34:17 -0400

    chore(mobile): add welcome page step 1

--- src/app/assets/image/index.ts
+++ src/app/assets/image/index.ts
@@ -1,6 +1,7 @@
 export const images = {
   bg_wallpaper: require('./source/bg.png'),
   default: require('./source/default.png'),
+  welcome_tiktok_logo: require('./source/welcome_tiktok_logo.png'),
 };
 
 export type ImageTypes = keyof typeof images;
--- src/app/features/un-authentication/welcome/index.tsx
+++ src/app/features/un-authentication/welcome/index.tsx
@@ -1,21 +1,40 @@
-import React, { memo, useRef, useState } from 'react';
+import React, { memo, useRef, useState, useCallback } from 'react';
+import { View } from 'react-native';
 
 import isEqual from 'react-fast-compare';
 
 import { dispatch } from '@common';
-import { Block, Screen, Text } from '@components';
+import { Block, LocalImage, Screen, Text } from '@components';
 import { useAnimatedState } from '@hooks';
 
 const WelcomeComponent = () => {
   return (
     <Block block paddingTop={0} paddingHorizontal={15}>
       <Screen
+        statusBarStyle="dark-content"
         bottomInsetColor="transparent"
-        scroll
         style={{ paddingVertical: 0, paddingHorizontal: 10 }}
         backgroundColor={'transparent'}>
-        <Block paddingVertical={15} middle direction={'row'}>
-          <Text>this is welcome component</Text>
+        <Block paddingVertical={15} direction={'column'}>
+          <Text fontSize={36} fontWeight="bold">
+            Videos to
+          </Text>
+          <Text fontSize={36} fontWeight="bold">
+            Make
+          </Text>
+          <Text fontSize={36} fontWeight="bold">
+            Your Day
+          </Text>
+        </Block>
+        <Block position={'absolute'} left={0} bottom={30}>
+          <View
+            pointerEvents={'none'}
+            style={{
+              width: 140,
+              height: 140,
+            }}>
+            <LocalImage resizeMode={'contain'} source={'welcome_tiktok_logo'} />
+          </View>
         </Block>
       </Screen>
     </Block>
